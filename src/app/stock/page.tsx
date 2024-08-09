ef6e88bf2f5683b22bd3e6a8e55a36b284e3d0b2
rhdholariya <rahuldholariya001@gmail.com>
2024-08-09 19:16:48 +0530

    new page

--- src/app/stock/page.tsx
+++ src/app/stock/page.tsx
@@ -1,6 +1,7 @@
 import { StockChart } from '@/components/GlobalTop5Stocks/GlobalStockChart/StockChart';
 import DefaultLayout from '@/components/Layouts/DefaultLayout';
-import TopFiveGlobalStocks from '@/components/TopFiveStocks';
+import StockHistoryTable from '@/components/StockHistoryTable';
+import stocks from "@/fakeData/top5Stocks.json";
 import React from 'react'
 
 const Stock = () => {
@@ -8,7 +9,7 @@ const Stock = () => {
     <DefaultLayout>
         <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
             <div className='col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8'>
-                <TopFiveGlobalStocks />
+                <StockHistoryTable stocks={stocks.top5Stocks} />
             </div>
             <div className="col-span-12 rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
            
